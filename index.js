e6eeb7a7de671d72a3cb3f2ec6bcc3d2a5ad102a
marcellobc <46097069+marcellobc@users.noreply.github.com>
2021-11-18 19:07:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,200 @@
+const cores = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGrey",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkSlateGrey",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Grey",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSlateGrey",
+  "LightSteelBlue",
+  "LightYellow",
+  "Lime",
+  "LimeGreen",
+  "Linen",
+  "Magenta",
+  "Maroon",
+  "MediumAquaMarine",
+  "MediumBlue",
+  "MediumOrchid",
+  "MediumPurple",
+  "MediumSeaGreen",
+  "MediumSlateBlue",
+  "MediumSpringGreen",
+  "MediumTurquoise",
+  "MediumVioletRed",
+  "MidnightBlue",
+  "MintCream",
+  "MistyRose",
+  "Moccasin",
+  "NavajoWhite",
+  "Navy",
+  "OldLace",
+  "Olive",
+  "OliveDrab",
+  "Orange",
+  "OrangeRed",
+  "Orchid",
+  "PaleGoldenRod",
+  "PaleGreen",
+  "PaleTurquoise",
+  "PaleVioletRed",
+  "PapayaWhip",
+  "PeachPuff",
+  "Peru",
+  "Pink",
+  "Plum",
+  "PowderBlue",
+  "Purple",
+  "RebeccaPurple",
+  "Red",
+  "RosyBrown",
+  "RoyalBlue",
+  "SaddleBrown",
+  "Salmon",
+  "SandyBrown",
+  "SeaGreen",
+  "SeaShell",
+  "Sienna",
+  "Silver",
+  "SkyBlue",
+  "SlateBlue",
+  "SlateGray",
+  "SlateGrey",
+  "Snow",
+  "SpringGreen",
+  "SteelBlue",
+  "Tan",
+  "Teal",
+  "Thistle",
+  "Tomato",
+  "Turquoise",
+  "Violet",
+  "Wheat",
+  "White",
+  "WhiteSmoke",
+  "Yellow",
+  "YellowGreen",
+];
+
+const menorOuMaior = (posicaoCor, posicaoEscolha) =>
+  posicaoCor > posicaoEscolha ? "inferior" : "superior";
+
+const checarOrdemAlfabetica = (arr, resposta, escolha) => {
+  posicaoCor = arr.indexOf(resposta);
+  posicaoEscolha = arr.indexOf(escolha);
+
+  if (posicaoCor === -1 || posicaoEscolha === -1) {
+    return null;
+  }
+  return menorOuMaior(posicaoCor, posicaoEscolha);
+};
+
+let coresSelecionadas = [];
+
+for (let i = 0; i < 10; i++) {
+  const index = Math.floor(Math.random() * cores.length);
+  coresSelecionadas = [...coresSelecionadas, cores[index]];
+}
+
+coresSelecionadas = coresSelecionadas.sort();
+
+const index = Math.floor(Math.random() * 10);
+
+var resposta = coresSelecionadas[index];
+
+console.log(resposta);
+
+let disponiveis = "| ";
+for (cor of coresSelecionadas) {
+  disponiveis += cor + " | ";
+}
+
+do {
+  var escolha = prompt(
+    `${disponiveis} \n\nQual será a cor que estou pensando?`
+  );
+  const ordem = checarOrdemAlfabetica(coresSelecionadas, resposta, escolha);
+
+  if (!ordem) {
+    alert(`Escolhe direto!`);
+  } else if (resposta.toLowerCase() !== escolha.toLowerCase()) {
+    alert(
+      `Errowwww!\n Dica: sua cor é alfabeticamente ${ordem} à minha.\n Tente novamente :)`
+    );
+  }
+} while (resposta.toLowerCase() !== escolha.toLowerCase());
+
+alert("Parabéns, meu chapaa!!");
